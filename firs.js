43d8a63c05feb5cbf7a15c480fc884f2ce0d5656
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-03-22 19:57:04 +0430

    firstJS

---
+++ firs.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const server = http.createServer((req, res)=>{
+  if(req.url === /){
+    res.end(" this is our home page darling ^_^ ")
+
+  }
+  if(req.url === /about){
+    res.end(" are you lost babe? ¬_¬ ")
+  }
+  res.end(`
+    <h1> Oops !!</h1>
+    <p> seems you are lost ^_~ </p>
+    <a href = '/'> back home </a>
+    `)
+})
