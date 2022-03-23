5364ccabb645ee83ad08062941a82e41af9a929c
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-03-23 20:05:52 +0430

    bit

--- firs.js
+++ firs.js
@@ -150,19 +150,6 @@ customEmitter.emit('response', 'john', 34)
 
 
 
-// ****************examples by John Smilga*******************************
 
 
-const { createReadStream } = require('fs')
 
-// default 64kb
-// last buffer - remainder
-// highWaterMark - control size
-// const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 })
-// const stream = createReadStream('../content/big.txt', { encoding: 'utf8' })
-const stream = createReadStream('./content/big.txt')
-
-stream.on('data', (result) => {
-  console.log(result)
-})
-stream.on('error', (err) => console.log(err))
