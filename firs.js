b74e26878d7a4c547782262029b10a34fd3d9705
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-03-23 19:40:50 +0430

    event.

--- firs.js
+++ firs.js
@@ -96,4 +96,33 @@ starter()
 
 
 
-// ! events in node ===========================================================================
+// ! events in node ==================================================================================
+
+
+
+
+// ****************examples by John Smilga*******************************
+
+// get back the class
+// if want custom extend from class
+// otherwise just for emitting and handling events create instance
+const EventEmitter = require('events')
+//  its a class
+const customEmitter = new EventEmitter()
+
+// on and emit methods
+// keep track of the order
+// additional arguments
+// built-in modules utilize it
+
+
+//  the order is important (first "on" then "emit", the first "data received ..." then "some other logic...")
+customEmitter.on('response', (name, id) => {
+  console.log(`data recieved user ${name} with id:${id}`)
+})
+
+customEmitter.on('response', () => {
+  console.log('some other logic here')
+})
+
+customEmitter.emit('response', 'john', 34)
