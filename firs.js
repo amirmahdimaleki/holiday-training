2456ccab072a3357d7dc860a3bbd527e54f536cc
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-03-23 20:05:09 +0430

    stream

--- firs.js
+++ firs.js
@@ -126,3 +126,43 @@ customEmitter.on('response', () => {
 })
 
 customEmitter.emit('response', 'john', 34)
+
+// ******************** request events *************************************
+
+
+//* const http = require('http')
+
+// const server = http.createServer((req, res) => {
+//   res.end('Welcome')
+// })
+
+// Using Event Emitter API
+//* const server = http.createServer()
+// emits request event
+// subscribe to it / listen for it / respond to it
+//* server.on('request', (req, res) => {
+//*   res.end('Welcome')
+//* })
+
+//* server.listen(5000)
+
+// !  streams ========================================================================================
+
+
+
+// ****************examples by John Smilga*******************************
+
+
+const { createReadStream } = require('fs')
+
+// default 64kb
+// last buffer - remainder
+// highWaterMark - control size
+// const stream = createReadStream('./content/big.txt', { highWaterMark: 90000 })
+// const stream = createReadStream('../content/big.txt', { encoding: 'utf8' })
+const stream = createReadStream('./content/big.txt')
+
+stream.on('data', (result) => {
+  console.log(result)
+})
+stream.on('error', (err) => console.log(err))
